c4c7dcdce9d6640b892cc52d8137a46e5ccc5cfa
kenta-matsuzaki <kenta6910@gmail.com>
2020-06-10 11:23:31 +0900

    test

--- Java_1/Main.java
+++ Java_1/Main.java
@@ -1,14 +1,12 @@
-import java.util.Scanner;
+// test
+public class Main {
+  public static int add(int x , int y){
+    int ans = x + y;
+    return ans;
+  }
 
-class Main {
   public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-    
-    System.out.print("一言入力してください：");
-    String inputText = scanner.next();
-
-    System.out.println("入力結果：" + inputText);
+    int ans = add(100, 10);
+    System.out.println("100 + 10 = " + ans);
   }
 }
-
-
